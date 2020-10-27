8810a4562d764bbd2e31240f22c397f642056602
sunithaaa <suninavi45@gmail.com>
2020-10-27 16:58:51 +0530

    complete todo app with react context api

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,43 @@
+import React, {useContext, useState} from "react"
+import {
+    FormGroup, Input, Button, Form, InputGroup, InputGroupAddon
+} from "reactstrap"
+import {v4} from "uuid"
+import {TodoContext} from "../context/TodoContext"
+import {ADD_TODO} from "../context/action.types"
+
+const TodoForm = () => {
+    const [todoString, setTodoString]=useState("");
+    const { dispatch } = useContext(TodoContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(todoString===""){
+            alert("please enter a todo")
+        }
+        const todo = {
+            todoString,
+            id:v4()
+        }
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        });
+
+        setTodoString("");
+    }
+
+    return(
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <InputGroup>
+                    <Input type="text" name="todo" id="todo" placeholder="Enter New ToDo" value={todoString} onChange={e => setTodoString(e.target.value)} />
+                    <InputGroupAddon addonType="prepend">
+                    <Button color="success">Add</Button>
+                </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+}
+export default TodoForm;
